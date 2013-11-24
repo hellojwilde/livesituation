19008380e5441a4a06b90500bde07dd233de8685
Jonathan Wilde <hello@jwilde.me>
2013-11-23 20:44:48 -0800

    Updating changeset.js to use the new Place and Change.

--- electro/lib/changeset.js
+++ electro/lib/changeset.js
@@ -1,28 +1,16 @@
-class Revision {
-  constructor(sequenceId, data) {
-    this._sequenceId = sequenceId || 1;
-    this._data = data || {};
-  }
-
-  get sequenceId() { return this._sequenceId; }
-  get data() { return this._data; }
-}
+var {Change} = require("./change");
+var {Revision} = require("./document");
 
 class Changeset {
   constructor (baseSequenceId, changes) {
     this._baseSequenceId = aBaseRevision;
     this._changes = changes || [];
-    if (!this._changes.every(Change.isValid))
-      throw new Error("Changeset: change in set is invalid");
   }
 
   get baseSequenceId() { return this._baseSequenceId; }
   get changes() { return this._changes; }
 
-  push(change) {
-    if (!Change.isValid(change))
-      throw new Error("Changeset.push: change is invalid");
-
+  push(change) { 
     this._changes.push(change);
   }
 
@@ -37,20 +25,20 @@ class Changeset {
     }
 
     var cloneWithChanges = this.changes.reduce((data, change) =>
-      Change.mutate(change, data), Util.cloneObject(revision.data));
+      change.mutate(data), Util.cloneObject(revision.data));
 
     return new Revision(cloneWithChanges, revision.sequenceId + 1)
   }
 
   invert() {
     return new Changeset(this.changes.reduceRight(
-      (inverted, change) => inverted.concat(Change.invert(change)), []));
+      (inverted, change) => inverted.concat(change.inverted), []));
   }
 
   transform(changeset) {
     var transformByChangeset = (toTransform) => 
       changeset.changes.reduce((transforming, change) => 
-        Change.transform(change, transforming), toTransform);
+        change.transform(transforming), toTransform);
 
     return new Changeset(this.changes.map(transformByChangeset));
   }  
