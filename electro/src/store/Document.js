1226904f0b78a679230217801af68b254a2e29af
Jonathan Wilde <hello@jwilde.me>
2014-01-28 01:13:58 +0800

    Revert "switching the cursor storage over to mori hashmaps."
    This reverts commit a35943ddf9808a03a420006f05641c0ca884ddee.

--- electro/src/store/Document.js
+++ electro/src/store/Document.js
@@ -1,17 +1,28 @@
 "use strict";
 
 var _ = require("underscore");
-var mori = require("mori");
-
 var EventEmitter = require("events").EventEmitter;
 var Revision = require("../core/Revision");
 var MessageType = require("../core/Wire").MessageType;
+
+// TODO (jwilde): What order should the place and client arguments be?
+//                It's all rather inconsistent right now across the codebase.
+
+function Cursor(place, client) {
+  this._place = place;
+  this._client = client;
+}
+
+Cursor.prototype = {
+  getPlace: function () { return this._place; },
+  getClient: function () { return this._client; }
+};
 
 function Document(initialData) {
   EventEmitter.call(this);
   this._baseRevision = new Revision(0, initialData || {});
   this._changesets = [];
-  this._cursors = mori.hash_map();
+  this._cursors = [];
 }
 
 Document.prototype = _.extend({
@@ -29,14 +40,26 @@ Document.prototype = _.extend({
     return this._changesets.slice(start || 0, end);
   },
 
-  getCursor: function (client) {
-    return mori.get(this._cursors, client);
+  // TODO (jwilde): The entire cursor data storage architecture here is going
+  //                to be really slow when there are large numbers of users.
+  //                Can we somehow get and use a Map datastructure polyfill?
+
+  getCursors: function () {
+    return this._cursors;
   },
 
-  setCursor: function (client, newPlace) {
-    var oldPlace = this.getCursor(client);
-    this._cursors = mori.assoc(this._cursors, client, newPlace);
-    this.emit(MessageType.CursorMove, oldPlace, newPlace, client);
+  getCursorForClient: function (client) {
+    return _.find(this._cursors, function (cursor) { 
+      return client == cursor.getClient(); 
+    });
+  },
+
+  setCursorForClient: function (client, place) {
+    var oldCursor = this.getCursorForClient(client);
+    var newCursor = new Cursor(place, client);
+
+    this._cursors = _.without(this._cursors, oldCursor).concat(newCursor);
+    this.emit(MessageType.CursorMove, newCursor, oldCursor, client);
   },
 
   commit: function (baseSequenceId, changeset, committer) {
